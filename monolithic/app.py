68e9ba7fd423749cfbeb69e40b00ab1946be3b0e
cavalcantej <joncav.12@gmail.com>
2020-11-09 17:25:56 -0300

    chat app working

--- monolithic/app.py
+++ monolithic/app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO, join_room, emit, send
+from flask_socketio import SocketIO, join_room, emit, send, leave_room
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user           
-from db import get_user, save_room, save_user
+from db import get_user, save_room, save_user, add_room_members, get_room_for_user, get_room_members, get_room, is_room_member, update_room, save_message, get_messages
 from user import User
-
+from datetime import datetime
+from bson.json_util import dumps
 
 app = Flask(__name__)
-app.secret_key = 'my secret key'
+app.config['SECRET_KEY'] = 'something123'
 socketio = SocketIO(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
- 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    rooms = []
+    if current_user.is_authenticated:
+        rooms = get_room_for_user(current_user.username)
+    return render_template("index.html", rooms=rooms)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -76,18 +81,57 @@ def create_room():
         else:
             message = "Failed to create room"
 
-    return render_template('create_room.html')
+    return render_template('create_room.html', message=message)
 
-@app.route('/chat')
-def chat(): 
-    username = request.args.get('username')
-    room = request.args.get('room_name')
-    
+@app.route('/rooms/<room_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_room(room_id):
+    room = get_room(room_id)
+    if room and is_room_member(room_id, current_user.username):
+        existing_room_members = [member['_id']['username'] for member in get_room_members(room_id)]
+        
+        message = ''
+        if request.method == 'POST':
+            room_name = request.form.get('room_name')
+            room['name'] = room_name
+            room_member_str = ",".join(existing_room_members)
+            update_room(room_id, room_name)
 
-    if username and room:
-        return render_template('chat.html', username=username, room=room)
+            new_members = [username.strip() for username in request.form.get('members').split(',')]        
+            members_to_add = list(set(new_members) - set(existing_members))
+            members_to_remove = list(set(existing_room_members) - set(new_members))
+            if len(members_to_add):
+                add_room_members(room_id, room_name, members_to_add, current_user.username)
+            if len(members_to_remove):
+                remove_room_members(room_id, members_to_remove)
+            message = 'Room edited successfully'
+            room_member_str = ",".join(new_members)
+        return render_template('edit_room.html', room=room, room_member_str=room_member_str)
     else:
-        return redirect(url_for('home'))
+        return "Room not found", 404
+
+@app.route('/rooms/<room_id>/messages/')
+@login_required
+def get_older_messages(room_id):
+    room = get_room(room_id)
+    if room and is_room_member(room_id, current_user.username):
+        page = int(request.args.get('page', 0))
+        messages = get_messages(room_id, page)
+        return dumps(messages)
+    else:
+        return "Room not found", 404
+
+@app.route('/rooms/<room_id>/')
+@login_required
+def view_room(room_id):
+    room = get_room(room_id)
+    if room and is_room_member(room_id,current_user.username):
+        room_members = get_room_members(room_id)
+        messages = get_messages(room_id)
+        return render_template('view_room.html',username=current_user.username, room=room, 
+                                room_members=room_members, messages=messages)
+    else:
+        return "Room not found", 404
 
 @socketio.on('send_message')
 def hangle_send_message_event(data):
@@ -95,11 +139,13 @@ def hangle_send_message_event(data):
                                                                     data['room'], 
                                                                     data['message']))
     # message to all room
+    data['created_at'] = datetime.now().strftime("%d %b, %H:%M")
+    save_message(data['room'], data['message'], data['username'])
     socketio.emit('receive_message', data, room=data['room'])
 
 @socketio.on('leave_room')
 def handle_leave_room_event(data):
-    app.loggerinfo('{} has left the room {}'.format(data['username'], data['room']))
+    app.logger.info('{} has left the room {}'.format(data['username'], data['room']))
     leave_room(data['room'])
     socketio.emit('leave_room_announcement', data, room=data['room'])
 
@@ -112,7 +158,6 @@ def handle_join_room_event(data):
 @login_manager.user_loader
 def load_user(username):
     return get_user(username)
-
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
